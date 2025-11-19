f6addf2e93b94c9372b865888e7c99c413aa42af
Syed Ali Murtaza <ali@kazmi.me>
2025-11-19 10:34:22 +0500

    transform to signal

--- src/lib/form/repeatable/repeatable-form-base.component.ts
+++ src/lib/form/repeatable/repeatable-form-base.component.ts
@@ -1,21 +1,27 @@
-import { Component, EventEmitter, Input, Output, ChangeDetectionStrategy } from '@angular/core';
+import { Component, EventEmitter, Output, ChangeDetectionStrategy, input, Signal, computed } from '@angular/core';
 import { FormBaseComponent } from '../form-base.component';
 import { IHasForm } from '../../base.types';
 
 @Component({
-    template: '',
-    changeDetection: ChangeDetectionStrategy.OnPush,
-    standalone: false
+  template: '',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: false,
 })
-export abstract class RepeatableFormBaseComponent<TEntity extends IHasForm<TEntity>> extends FormBaseComponent<TEntity>{
-  @Input() data: TEntity;
-
-  @Input() index: number;
-
+export abstract class RepeatableFormBaseComponent<TEntity extends IHasForm<TEntity>>
+  extends FormBaseComponent<TEntity>
+{
+  // --- INPUTS (signals) ---
+  readonly data = input<TEntity>();
+  readonly index = input<number>();
+  
+  // --- OUTPUT ---
   @Output() dataChange = new EventEmitter<TEntity>();
 
+  // Optional computed if you want a stable non-null entity to work with
+  readonly dataComputed: Signal<TEntity | undefined> = computed(() => this.data());
+
   protected emitChange() {
-    if (this.dataChange && this.formGroup.valid) {
+    if (this.formGroup.valid) {
       this.dataChange.emit(this.getEntityFromForm() as TEntity);
     }
   }
